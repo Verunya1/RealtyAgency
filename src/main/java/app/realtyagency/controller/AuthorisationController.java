ef965068e5d8d3f6f7b406b59431da88e6f2c51e
vera3 <vera3899@gmail.com>
2023-06-11 16:11:19 +0300

    front update

--- src/main/java/app/realtyagency/controller/AuthorisationController.java
+++ src/main/java/app/realtyagency/controller/AuthorisationController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.*;
 public class AuthorisationController {
     private final UserService userService;
 
-
-
     @GetMapping("register")
     public String register(Model model) {
         model.addAttribute("user", new User());
--- src/main/java/app/realtyagency/controller/RealtyController.java
+++ src/main/java/app/realtyagency/controller/RealtyController.java
@@ -6,16 +6,19 @@ import app.realtyagency.entity.User;
 import app.realtyagency.service.RealtiesService;
 import app.realtyagency.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -26,8 +29,8 @@ public class RealtyController {
     private final UserService userService;
 
     @GetMapping("/index")
-    public String getAdvertisement( Model model,Authentication authentication) {
-        if (authentication!=null) {
+    public String getAdvertisement(Model model, Authentication authentication) {
+        if (authentication != null) {
             User user = (User) authentication.getPrincipal();
             model.addAttribute("user", user);
         }
@@ -38,6 +41,7 @@ public class RealtyController {
             users.add(userService.get(realty.getUserId()));
             realtyImages.add(realtyService.getAllImages(realty.getId()).get(0));
         });
+        users.addAll(realties.stream().map(r -> userService.get(r.getUserId())).collect(Collectors.toList()));
         model.addAttribute("ads", realties);
         model.addAttribute("users", users);
         model.addAttribute("images", realtyImages);
@@ -45,12 +49,12 @@ public class RealtyController {
     }
 
     @GetMapping("/advertisement")
-    public String advertisementInfo(@RequestParam("id") Long id, Model model,Authentication authentication) {
+    public String advertisementInfo(@RequestParam("id") Long id, Model model, Authentication authentication) {
         Realty realty = realtyService.getRealtyByID(id);
         User owner = userService.get(realty.getUserId());
-        if (authentication!=null) {
+        if (authentication != null) {
             User user = (User) authentication.getPrincipal();
-            model.addAttribute("user",user);
+            model.addAttribute("user", user);
         }
         List<RealtyImage> realtyImages = realtyService.getAllImages(id);
         model.addAttribute("advertisement", realty);
@@ -60,9 +64,9 @@ public class RealtyController {
     }
 
     @GetMapping("/advertisement/new")
-    public String newAdvertisement(@ModelAttribute("advertisement") Realty realty,Authentication authentication,Model model) {
-        User user = (User)authentication.getPrincipal();
-        model.addAttribute("user",user);
+    public String newAdvertisement(@ModelAttribute("advertisement") Realty realty, Authentication authentication, Model model) {
+        User user = (User) authentication.getPrincipal();
+        model.addAttribute("user", user);
         return "add_advertisement";
     }
 
@@ -71,22 +75,22 @@ public class RealtyController {
                                       @RequestParam("file3") MultipartFile file3, @ModelAttribute("advertisement") Realty realty, Authentication authentication)
             throws IOException {
 
-        User user = (User)authentication.getPrincipal();
+        User user = (User) authentication.getPrincipal();
         realty.setUserId(user.getId());
         realtyService.saveRealty(realty, file1, file2, file3);
         return "redirect:/index";
     }
 
     @GetMapping("/advertisement/delete")
-    public String deleteAdvertisement(@RequestParam("id") Long id){
+    public String deleteAdvertisement(@RequestParam("id") Long id) {
         realtyService.deleteRealty(id);
         return "redirect:/index";
     }
 
     @GetMapping("/advertisement/buy")
-    public String buyAdvertisement(@RequestParam("id") Long id,Authentication authentication){
-        User user = (User)authentication.getPrincipal();
-        userService.buyRealtyById(user,id);
+    public String buyAdvertisement(@RequestParam("id") Long id, Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        userService.buyRealtyById(user, id);
         return "redirect:/index";
     }
 
